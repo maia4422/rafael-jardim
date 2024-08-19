7b5745c72b9812dcf27c89286a56da49c90c8036
Matheus Peixoto <matheus.peixoto@townsq.com.br>
2024-08-19 14:45:51 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import foto from "./assets/rafaelsozinho.png";
+import nomenumero from "./assets/nomenumero.png";
+import emconstrucao from "./assets/emconstruçao.webp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		document.title = "Rafael Jardim";
+	}, []);
+
+	return (
+		<div id="parent">
+			<header id="header">RAFAEL JARDIM</header>
+			<main>
+				<div>
+					<img
+						id="fotoemconstrucao"
+						src={emconstrucao}
+						alt="Site em construção"
+					/>
+					<img
+						id="fotonumero1"
+						src={nomenumero}
+						alt="23023 Rafael Jardim"
+					/>
+				</div>
+
+				<img id="fotorafael" src={foto} alt="Foto do Rafael Jardim" />
+				<img
+					id="fotonumero2"
+					src={nomenumero}
+					alt="23023 Rafael Jardim"
+				/>
+			</main>
+		</div>
+	);
 }
 
 export default App;
